eb361bf90aef048e294122fa5377eefb3276ae78
karnmanoo <karnmanoo@yahoo.com>
2021-07-29 02:16:32 +0700

    Create Lecture97 Homework
    BorntoDev
    Course : C#
    Lecture : 97
    Homework : SELECT

---
+++ Csharp_Lecture97_SELECT/Csharp_Lecture97_SELECT/DataAccess.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Csharp_Lecture97_SELECT
+{
+    class DataAccess
+    {
+        public const string dbpath = "Customers.db"; //ประกาศตัวแปรแทน Database ชื่อ Customers
+        public static void InitializeDatabase()
+        {
+            using (SqliteConnection db = new SqliteConnection($"Filename={dbpath}"))
+            {
+                db.Open();
+                String tableCommand = "CREATE TABLE IF NOT " +
+                    "EXISTS Customers (uid INTEGER PRIMARY KEY, " +
+                    "first_Name VARCHAR(255) NULL," +
+                    "last_Name VARCHAR(255) NULL," +
+                    "email VARCHAR(255) NULL)";
+                SqliteCommand createTable = new SqliteCommand(tableCommand, db);
+                createTable.ExecuteReader();
+            }
+        }
+        public static void AddData(string uid, string firstName, string lastName, string email)
+        {
+            using (SqliteConnection db = new SqliteConnection($"Filename={dbpath}"))
+            {
+                db.Open();
+                SqliteCommand insertCommand = new SqliteCommand();
+                insertCommand.Connection = db;
+
+                // Use parameterized query to prevent SQL injection attacks
+                insertCommand.CommandText = "INSERT INTO Customers VALUES (@uid,@first_Name,@last_Name,@email);";
+                insertCommand.Parameters.AddWithValue("@uid", uid);
+                insertCommand.Parameters.AddWithValue("@first_Name", firstName);
+                insertCommand.Parameters.AddWithValue("@last_Name", lastName);
+                insertCommand.Parameters.AddWithValue("@email", email);
+                insertCommand.ExecuteReader();
+                db.Close();
+            }
+        }
+        public static List<String> GetData()
+        {
+            List<String> entries = new List<string>();
+            using (SqliteConnection db = new SqliteConnection($"Filename={dbpath}"))
+            {
+                db.Open();
+                SqliteCommand selectCommand = new SqliteCommand("SELECT uid,first_Name,last_Name,email from Customers", db);
+                //ถ้า SELECT ทุก Field ให้ใช้ SELECT * from ...
+                SqliteDataReader query = selectCommand.ExecuteReader();
+                while (query.Read())
+                {
+                    entries.Add(query.GetString(0));
+                    entries.Add(query.GetString(1));
+                    entries.Add(query.GetString(2));
+                    entries.Add(query.GetString(3));
+                }
+                db.Close();
+            }
+            return entries;
+        }
+        public static void ClearData()
+        {
+            using (SqliteConnection db = new SqliteConnection($"Filename={dbpath}"))
+            {
+                db.Open();
+                SqliteCommand clearCommand = new SqliteCommand();
+                clearCommand.Connection = db;
+                clearCommand.CommandText = "DELETE from Customers";
+                clearCommand.ExecuteNonQuery();
+                db.Close();
+            }
+        }
+    }
+}
---
+++ Csharp_Lecture97_SELECT/Csharp_Lecture97_SELECT/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Csharp_Lecture97_SELECT
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataAccess.InitializeDatabase();
+        }
+
+        private void addBtn_Click(object sender, RoutedEventArgs e)
+        {
+            DataAccess.AddData(uidTxt.Text, firstNameTxt.Text, lastNameTxt.Text, emailTxt.Text);
+            MessageBox.Show("Add customer data" + "\n" +
+                "Customer ID : " + uidTxt.Text + "\n" +
+                "First Name : " + firstNameTxt.Text + "\n" +
+                "Last Name : " + lastNameTxt.Text + "\n" +
+                "Email Address : " + emailTxt.Text,"Add cutomer data");
+            uidTxt.Clear();
+            firstNameTxt.Clear();
+            lastNameTxt.Clear();
+            emailTxt.Clear();
+        }
+
+        private void showBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string customerShow = "";
+            foreach(string data in DataAccess.GetData())
+            {
+                customerShow = customerShow + data + "\n";
+            }
+            MessageBox.Show("Customer Data as below:" + "\n" + customerShow, "Show all customer data");
+        } 
+
+        private void clearBtn_Click(object sender, RoutedEventArgs e)
+        {
+            DataAccess.ClearData();
+            uidTxt.Clear();
+            firstNameTxt.Clear();
+            lastNameTxt.Clear();
+            emailTxt.Clear();
+            MessageBox.Show("Customer Data has been cleared","Clear customer data");
+        }
+    }
+}
